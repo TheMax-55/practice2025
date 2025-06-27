87a487cfb60611de9c92d3a9266484cf704d0c72
Maxim Leonkin <maksimleonkin583@gmail.com>
2025-06-27 16:18:02 +0600

    feat: add extension methods realisation and tests

---
+++ task01/task01.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace task01
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+            input = input.ToLower();
+            string cleaned = string.Empty;
+            string reversed = string.Empty;
+            foreach (char sym in input)
+            {
+                if (!Char.IsWhiteSpace(sym) && !Char.IsPunctuation(sym))
+                {
+                    cleaned += sym;
+                    reversed = sym + reversed;
+                }
+            }
+            if (string.IsNullOrEmpty(cleaned))
+                return false;
+            return cleaned == reversed;
+        }
+    }
+}
