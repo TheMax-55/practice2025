9dce75a80eebc0e3198baf3bf1ba587c69d847a7
Maxim Leonkin <maksimleonkin583@gmail.com>
2025-06-29 01:52:46 +0600

    feat: add ClassAnalyzer and tests

---
+++ task05tests/task05tests.cs
@@ -0,0 +1,74 @@
+﻿using Xunit;
+using Moq;
+using task05;
+
+public class TestClass
+{
+    public int PublicField;
+    private string _privateField = string.Empty;
+    public int Property { get; set; }
+
+    public void Method() { }
+    public void MethodWithParams(int a, int b) { }
+}
+
+[Serializable]
+public class AttributedClass { }
+
+public class ClassAnalyzerTests
+{
+    [Fact]
+    public void GetPublicMethods_ReturnsCorrectMethods()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var methods = analyzer.GetPublicMethods();
+
+        Assert.Contains("Method", methods);
+    }
+
+    [Fact]
+    public void GetMethodParams_MethodWithoutParameters()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var parameters = analyzer.GetMethodParams("Method");
+
+        Assert.Empty(parameters);
+    }
+
+    [Fact]
+    public void GetMethodParams_MethodWithParameters()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var parameters = analyzer.GetMethodParams("MethodWithParams");
+
+        Assert.Contains("a", parameters);
+        Assert.Contains("b", parameters);
+    }
+
+    [Fact]
+    public void GetAllFields_IncludesPrivateFields()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var fields = analyzer.GetAllFields();
+
+        Assert.Contains("_privateField", fields);
+    }
+
+    [Fact]
+    public void GetAllProperties_ReturnCorrectProperties()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var properties = analyzer.GetProperties();
+
+        Assert.Contains("Property", properties);
+    }
+
+    [Fact]
+    public void HasAttribute_ReturnsTrue()
+    {
+        var analyzer = new ClassAnalyzer(typeof(AttributedClass));
+        var attribute = analyzer.HasAttribute<SerializableAttribute>();
+
+        Assert.True(attribute);
+    }
+}
