2683a6b28d2e30c3344071b3b3c764be6383e254
Maxim Leonkin <maksimleonkin583@gmail.com>
2025-06-29 00:00:51 +0600

    feat: add Spaceship classes and tests

---
+++ task04/task04.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+namespace task04
+{
+    public interface ISpaceship
+    {
+        void MoveForward();
+        void Rotate(int angle);
+        void Fire();
+        int Speed { get; }
+        int FirePower { get; }
+    }
+    public class Cruiser : ISpaceship
+    {
+        public int Distance;
+        public int Angle;
+        public bool Shot;
+        public int Speed { get; } = 50;
+        public int FirePower { get; } = 100;
+        public void MoveForward() => Distance += Speed;
+        public void Rotate(int angle) => Angle = (Angle + angle) % 360;
+        public void Fire() => Shot = true; 
+        
+    }
+    public class Fighter : ISpaceship
+    {
+        public int Distance;
+        public int Angle;
+        public bool Shot;
+        public int Speed { get; } = 100;
+        public int FirePower { get; } = 50;
+        public void MoveForward() => Distance += Speed;
+        public void Rotate(int angle) => Angle = (Angle + angle) % 360;
+        public void Fire() => Shot = true; 
+    }
+}
