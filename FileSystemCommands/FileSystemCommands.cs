fc50c405b17ef4c01e3c2d20d05d8dd627c9e5bf
Maxim Leonkin <maksimleonkin583@gmail.com>
2025-07-10 01:34:47 +0600

    feat: file system commands and tests

---
+++ FileSystemCommands/FileSystemCommands.cs
@@ -0,0 +1,37 @@
+﻿using CommandLib;
+namespace FileSystemCommands;
+public class DirectorySizeCommand : ICommand
+{
+    private readonly string _path;
+    public long size { get; private set; }
+
+    public DirectorySizeCommand(string path)
+        => _path = path;
+
+    public void Execute()
+    {
+        DirectoryInfo directory = new DirectoryInfo(_path);
+        if (directory.Exists)
+            size = directory.GetFiles()
+            .Sum(f => f.Length);
+    }
+}
+
+public class FindFilesCommand : ICommand
+{
+    private readonly string _path;
+    private readonly string _mask;
+    public List<string> files { get; private set; } = new List<string>();
+
+    public FindFilesCommand(string path, string mask)
+        => (_path, _mask) = (path, mask);
+
+    public void Execute()
+    {
+        DirectoryInfo directory = new DirectoryInfo(_path);
+        if (directory.Exists)
+            files = directory.GetFiles(_mask)
+            .Select(f => f.Name)
+            .ToList();
+    }
+}
---
+++ task08/CommandRunner.cs
@@ -0,0 +1,42 @@
+﻿using CommandLib;
+using FileSystemCommands;
+using System.Reflection;
+namespace CommandRunner;
+
+public class Program
+{
+    public static void Main()
+    {
+        var assembly = Assembly.LoadFrom("FileSystemCommands.dll");
+
+        var testDir1 = Path.Combine(Path.GetTempPath(), "TestDir1");
+        Directory.CreateDirectory(testDir1);
+        File.WriteAllText(Path.Combine(testDir1, "test1.txt"), "Hello");
+        File.WriteAllText(Path.Combine(testDir1, "test2.txt"), "World");
+
+        Type? sizeCommandType = assembly.GetType("FileSystemCommands.DirectorySizeCommand");
+
+        if (sizeCommandType != null && Activator.CreateInstance(sizeCommandType, testDir1) is DirectorySizeCommand sizeCommand)
+        {
+            sizeCommand.Execute();
+            Console.WriteLine($"Размер каталога равен {sizeCommand.size}");
+        }
+
+        var testDir2 = Path.Combine(Path.GetTempPath(), "TestDir2");
+        Directory.CreateDirectory(testDir2);
+        File.WriteAllText(Path.Combine(testDir2, "file1.txt"), "Text");
+        File.WriteAllText(Path.Combine(testDir2, "file2.log"), "Log");
+
+        Type? findFilesType = assembly.GetType("FileSystemCommands.FindFilesCommand");
+
+        if (findFilesType != null && Activator.CreateInstance(findFilesType, testDir2, "*.txt") is FindFilesCommand filesCommand)
+        {
+            filesCommand.Execute();
+            foreach (var file in filesCommand.files)
+                Console.WriteLine(file);
+        }
+
+        Directory.Delete(testDir1, true);
+        Directory.Delete(testDir2, true);
+    }
+}
