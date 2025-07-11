7650d942a484c4d179b8c3592d1bfa1e03440c1b
Maxim Leonkin <maksimleonkin583@gmail.com>
2025-07-11 18:17:55 +0600

    feat: add dynamic calculator and tests

---
+++ task11/task11.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+namespace task11;
+
+public interface ICalculator
+{
+    int Add(int a, int b);
+    int Minus(int a, int b);
+    int Mul(int a, int b);
+    int Div(int a, int b);
+}
+
+public class CreateCalculator
+{
+    public static ICalculator Compile(string code)
+    {
+        var tree = CSharpSyntaxTree.ParseText(code);
+
+        var references = new MetadataReference[]
+        {
+            MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+            MetadataReference.CreateFromFile(typeof(ICalculator).Assembly.Location)
+        };
+
+        var compilation = CSharpCompilation
+            .Create("Calculator")
+            .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
+            .AddReferences(references)
+            .AddSyntaxTrees(tree);
+
+        var ms = new MemoryStream();
+        compilation.Emit(ms);
+
+        Assembly assembly = Assembly.Load(ms.ToArray());
+        Type? calculatorType = assembly.GetType("Calculator");
+        var calculatorInstance = Activator.CreateInstance(calculatorType!);
+        return (ICalculator)calculatorInstance!;
+    }
+}
---
+++ task11tests/task11tests.cs
@@ -0,0 +1,51 @@
+﻿using Xunit;
+using task11;
+
+public class CalculatorTests
+{
+    string code = @"
+    using task11;
+
+    public class Calculator : ICalculator
+    {
+        public int Add(int a, int b) => a + b;
+        public int Minus(int a, int b) => a - b;
+        public int Mul(int a, int b) => a * b;
+        public int Div(int a, int b) => a / b;
+    }";
+
+    [Fact]
+    public void Add_ShouldWorkCorrectly()
+    {
+        var calculator = CreateCalculator.Compile(code);
+        Assert.Equal(12, calculator.Add(7, 5));
+    }
+
+    [Fact]
+    public void Minus_ShouldWorkCorrectly()
+    {
+        var calculator = CreateCalculator.Compile(code);
+        Assert.Equal(-5, calculator.Minus(5, 10));
+    }
+
+    [Fact]
+    public void Mul_ShouldWorkCorrectly()
+    {
+        var calculator = CreateCalculator.Compile(code);
+        Assert.Equal(-56, calculator.Mul(7, -8));
+    }
+
+    [Fact]
+    public void Div_ShouldWorkCorrectly()
+    {
+        var calculator = CreateCalculator.Compile(code);
+        Assert.Equal(5, calculator.Div(15, 3));
+    }
+
+    [Fact]
+    public void DivByZero_ShouldThrowDivideByZeroException()
+    {
+        var calculator = CreateCalculator.Compile(code);
+        Assert.Throws<DivideByZeroException>(() => calculator.Div(1, 0));
+    }
+}
