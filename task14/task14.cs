719c6c71a993c72ae6a4e2a51dab18a30bb3fce7
Maxim Leonkin <maksimleonkin583@gmail.com>
2025-07-16 11:17:29 +0600

    feat: add definite integral solver and tests

---
+++ task14/task14.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+namespace task14;
+
+public class DefiniteIntegral
+{
+    public static double Solve(double a, double b, Func<double, double> function, double step, int threadsNumber)
+    {
+        double integral = 0;
+        double interval = (b - a) / threadsNumber;
+        Barrier barrier = new Barrier(threadsNumber + 1);
+        Thread[] threads = new Thread[threadsNumber];
+
+        for (int i = 0; i < threadsNumber; i++)
+        {
+            double start = a + i * interval;
+            double end = (i == threadsNumber - 1) ? b : start + interval;
+
+            threads[i] = new Thread(() =>
+            {
+                double localSum = Trapezoid(start, end, function, step);
+                Interlocked.Exchange(ref integral, integral + localSum);
+                barrier.SignalAndWait();
+            });
+            threads[i].Start();
+        }
+
+        barrier.SignalAndWait();
+        return integral;
+    }
+
+    static double Trapezoid(double start, double end, Func<double, double> function, double step)
+    {
+        double result = 0;
+        double current = start;
+
+        while (current < end)
+        {
+            double next = Math.Min(current + step, end);
+            double h = next - current;
+            result += (function(current) + function(next)) * h / 2;
+            current = next;
+        }
+
+        return result;
+    }
+}
---
+++ task14tests/task14tests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using task14;
+
+public class DefiniteIntegralTests
+{
+    [Fact]
+    public void DefiniteIntegral_LinearFunction_ShouldReturnCorrectResult()
+    {
+        var X = (double x) => x;
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, X, 1e-4, 2), 1e-4);
+    }
+
+    [Fact]
+    public void DefiniteIntegral_SinFunction_ShouldReturnCorrectResult()
+    {
+        var SIN = (double x) => Math.Sin(x);
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, SIN, 1e-5, 8), 1e-4);
+    }
+
+    [Fact]
+    public void DefiniteIntegral_PositiveLinearFunction_ShouldReturnCorrectResult()
+    {
+        var X = (double x) => x;
+        Assert.Equal(12.5, DefiniteIntegral.Solve(0, 5, X, 1e-6, 8), 1e-5);
+    }
+}
